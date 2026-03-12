bd9b5f510e6b22568c76d4b453920c6505d5d15d
main3782 <101455553+main3782@users.noreply.github.com>
2026-03-12 21:15:56 +0100

    Correct device-vendor.

--- IRL68/Texas Instruments PandaBoard (panda)/ti/panda/device-vendor.mk
+++ IRL68/Texas Instruments PandaBoard (panda)/ti/panda/device-vendor.mk
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/ti/panda/device-vendor-blobs.mk)
+$(call inherit-product, vendor/ti/panda/device-panda.mk)
